43dc0df0b7068a1d7bbe26a18f874771e6bc4608
MrNoell <noel.lelcaj@gmail.com>
2022-07-20 08:58:09 -0700

    added some dummy text, icons and head image:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import instagram from "./instagram.png";
+import github from "./github.png";
+import youtube from "./youtube.png";
+import header_img from "./header-image.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <img src={header_img} id="main-img"/>
+      <main>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie tempor lorem, eu commodo arcu molestie eget. Aliquam at tincidunt ex, quis semper ipsum. Mauris eleifend arcu purus. Nam ac dui elementum, facilisis est non, dignissim lectus. Nulla facilisi. Ut laoreet et metus vehicula sodales. Aenean egestas lectus diam, ut fermentum mi fringilla quis. Quisque feugiat tellus vel odio congue, a cursus metus fringilla. Etiam sagittis aliquam augue, ut lacinia justo commodo id. Suspendisse potenti. Morbi aliquam mollis lorem, in congue nisi maximus quis. Integer finibus euismod vehicula. Proin aliquet nisi sed risus dignissim, in molestie purus eleifend. Vestibulum et mollis mauris. Praesent viverra tincidunt turpis id tempor.
+      </main>
+      <div id="socials">
+        <a href="https://www.instagram.com/noel_lelcaj/">
+          <img src={instagram} className="icons"/>
         </a>
-      </header>
+        <a href="https://github.com/MrNoell">
+          <img src={github} className="icons"/>
+        </a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+          <img src={youtube} className="icons"/>
+        </a>
+      </div>
     </div>
   );
 }
