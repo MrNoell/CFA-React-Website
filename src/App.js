611fd386f747ad51311f23874ab30afd6259c749
MrNoell <noel.lelcaj@gmail.com>
2022-07-21 15:57:38 +0300

    Final styling touches

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import instagram from "./instagram.png";
 import github from "./github.png";
@@ -10,7 +9,15 @@ function App() {
     <div className="App">
       <img src={header_img} id="main-img"/>
       <main>
+      <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie tempor lorem, eu commodo arcu molestie eget. Aliquam at tincidunt ex, quis semper ipsum. Mauris eleifend arcu purus. Nam ac dui elementum, facilisis est non, dignissim lectus. Nulla facilisi. Ut laoreet et metus vehicula sodales. Aenean egestas lectus diam, ut fermentum mi fringilla quis. Quisque feugiat tellus vel odio congue, a cursus metus fringilla. Etiam sagittis aliquam augue, ut lacinia justo commodo id. Suspendisse potenti. Morbi aliquam mollis lorem, in congue nisi maximus quis. Integer finibus euismod vehicula. Proin aliquet nisi sed risus dignissim, in molestie purus eleifend. Vestibulum et mollis mauris. Praesent viverra tincidunt turpis id tempor.
+      </p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie tempor lorem, eu commodo arcu molestie eget. Aliquam at tincidunt ex, quis semper ipsum. Mauris eleifend arcu purus. Nam ac dui elementum, facilisis est non, dignissim lectus. Nulla facilisi. Ut laoreet et metus vehicula sodales. Aenean egestas lectus diam, ut fermentum mi fringilla quis. Quisque feugiat tellus vel odio congue, a cursus metus fringilla. Etiam sagittis aliquam augue, ut lacinia justo commodo id. Suspendisse potenti. Morbi aliquam mollis lorem, in congue nisi maximus quis. Integer finibus euismod vehicula. Proin aliquet nisi sed risus dignissim, in molestie purus eleifend. Vestibulum et mollis mauris. Praesent viverra tincidunt turpis id tempor.
+      </p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras molestie tempor lorem, eu commodo arcu molestie eget. Aliquam at tincidunt ex, quis semper ipsum. Mauris eleifend arcu purus. Nam ac dui elementum, facilisis est non, dignissim lectus. Nulla facilisi. Ut laoreet et metus vehicula sodales. Aenean egestas lectus diam, ut fermentum mi fringilla quis. Quisque feugiat tellus vel odio congue, a cursus metus fringilla. Etiam sagittis aliquam augue, ut lacinia justo commodo id. Suspendisse potenti. Morbi aliquam mollis lorem, in congue nisi maximus quis. Integer finibus euismod vehicula. Proin aliquet nisi sed risus dignissim, in molestie purus eleifend. Vestibulum et mollis mauris. Praesent viverra tincidunt turpis id tempor.
+      </p>
       </main>
       <div id="socials">
         <a href="https://www.instagram.com/noel_lelcaj/">
